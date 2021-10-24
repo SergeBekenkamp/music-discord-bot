2469d65f63497cfb973d71d7fb4bbb06c75d8bbd
Serge <sergebekenamp@outlook.com>
2021-10-25 01:18:53 +0200

    Add check if argument is a valid youtube video id

--- app/search/searchYoutube.ts
+++ app/search/searchYoutube.ts
@@ -1,5 +1,5 @@
 import search, {YouTubeSearchOptions, YouTubeSearchResults} from "youtube-search";
-import {streamYoutube} from "../integrations/youtube-stream/streamYoutube";
+import request from 'request-promise-native';
 
 const searchOptions: YouTubeSearchOptions = {
     maxResults: 10,
@@ -7,25 +7,29 @@ const searchOptions: YouTubeSearchOptions = {
 };
 
 export async function searchYoutube(searchValue: string): Promise<Partial<YouTubeSearchResults> | undefined> {
-    if(searchValue)
-    {
-        let isId = false;
-        if(searchValue.split(' ').length == 1) {
-            try {
-                await streamYoutube(`https://youtube.com/watch?v=${searchValue}`);
-                isId = true;
-            } catch (error) {
-                console.log(error);
-            }
-        }
+    const {
+        YOUTUBE_API_KEY
+    } = process.env;
 
-        const searchResults = await search(searchValue, searchOptions)
-        const exactMatch = searchResults.results.find(x=> x.id === searchValue);
-        return isId ? exactMatch ?? {
-            id: searchValue,
-            title: 'could not retrieve details',
-        }  : searchResults.results[0]
+
+    let isId = false;
+    if (searchValue.split(' ').length == 1) {
+        const result = await request(
+            `https://www.googleapis.com/youtube/v3/videos?part=id&id=${searchValue}&key=${YOUTUBE_API_KEY}`,
+            {json: true}
+        );
+
+        if (result?.items?.length === 1) {
+            isId = true;
+        }
     }
-    return undefined;
+
+    const searchResults = await search(searchValue, searchOptions)
+    const exactMatch = searchResults.results.find(x => x.id === searchValue);
+    return isId ? exactMatch ?? {
+        id: searchValue,
+        title: 'could not retrieve details',
+    } : searchResults.results.find(item => item.kind === 'youtube#video')
+
 
 }
